c53414f1c7e5399817be2ddc4bb840acca29e0dd
Pablo Cupertino <pabloagm7@gmail.com>
2024-01-19 17:52:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,11 @@
 package application;
 
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +13,12 @@ public class Program {
 		// criar o objeto Department
 		
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		
+		// criar um objeto para Seller
+		
+		Seller seller = new Seller(21,"Isis" , "isis@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
