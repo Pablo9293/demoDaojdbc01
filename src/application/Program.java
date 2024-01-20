96703746c073c96de2f18c297ea15a052b41a30b
Pablo Cupertino <pabloagm7@gmail.com>
2024-01-20 03:38:08 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,13 @@ public class Program {
 		//Instanciamos na linha de cima.Na linha de baixo vamos inserir no banco
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted ! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martin");
+		//salvando os dados do vendedor
+		sellerDao.update(seller);
+		System.out.println("Update completed ");
 
 
 
