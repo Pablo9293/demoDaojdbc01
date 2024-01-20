4303b49d10374d65d8f913471101a5daa4ee2af6
Pablo Cupertino <pabloagm7@gmail.com>
2024-01-19 21:02:12 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -18,6 +20,10 @@ public class Program {
 		
 		Seller seller = new Seller(21,"Isis" , "isis@gmail.com", new Date(), 3000.0, obj);
 		
+		// Acrescentado instanciação de SellerDao
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); // nao conhece a implemntação, somente a interface
+		// Acima e uma injeção de dependencia sem explicitar a implementacao.
 		System.out.println(seller);
 
 	}
